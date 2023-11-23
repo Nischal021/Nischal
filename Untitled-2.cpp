99866d4e7b0b61ea30d1ba186540d7245c9c5c48
Nischal021 <151807836+Nischal021@users.noreply.github.com>
2023-11-22 23:13:05 -0800

    Add files via upload

---
+++ Untitled-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=10,i;
+    cout<<"Enter a number"<<endl;
+    cin>>a;
+    if(a>5)
+{
+    cout<<"yes"<<endl;
+}
+else
+{
+    cout<<"NO"<<endl;
+}
+return 0;
+}
